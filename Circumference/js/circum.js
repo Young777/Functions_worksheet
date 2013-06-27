6e9a5b02f15f275aa767ec14337613f2a561904b
Shaylen Young <young777@fullsail.edu>
2013-06-26 23:22:51 -0500

    circumference fix

--- Circumference/js/circum.js
+++ Circumference/js/circum.js
@@ -3,8 +3,9 @@
     //Circumference of the circle
     var total = circleCircum(pi, diam)
 
+
+function circleCircum(3.14, 6){
     //Calculate Circumference
-    function circleCircum(3.14, 6){
     var Circumeference = pi * diam
 
     //Return Circumference
