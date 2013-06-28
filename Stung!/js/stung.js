25491c7de760753f44027ddf0fb2af5fa94ff334
Shaylen Young <young777@fullsail.edu>
2013-06-27 20:06:16 -0500

    stung correction

--- Stung!/js/stung.js
+++ Stung!/js/stung.js
@@ -1,8 +1,11 @@
 //How Many Stings - Shaylen Young - 06/26
 //Calculate Stings
 
+var Stingstotal = calculation
 //Weight of Lion
+
 var lionWeight = 550
 var stingsPer = 8.666666667
 
 
+function stingTotal
