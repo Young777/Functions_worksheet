7eae614cd2d31e50ff60bbae236f3a1738f9bcf3
Shaylen Young <young777@fullsail.edu>
2013-06-27 20:11:17 -0500

    sting final set up

--- Stung!/js/stung.js
+++ Stung!/js/stung.js
@@ -5,9 +5,7 @@ var Stingstotal = calculation(550, 8.666666667)
 
 
 //Weight of Lion * Stings Per
-function calculation
-var lionWeight = 550
-var stingsPer = 8.666666667
-
-
-function stingTotal
+function calculation (lionWeight, stingsPer){
+    var lionWeight = 550
+    var stingsPer = 8.666666667
+}
